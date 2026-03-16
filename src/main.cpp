3a0a7c968bbf8a755cfa72fe1a64bb33b74b3247
Grien25 <riengupta@gmail.com>
2026-03-15 22:47:39 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// adfus - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/adfus_config.h"
+#include "generated/adfus_init.h"
+
+#include "adfus_app.h"
+
+REX_DEFINE_APP(adfus, adfusApp::Create)
